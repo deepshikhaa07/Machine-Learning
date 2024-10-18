a735ae9d8438bf143834e03e7f29c07c017b81db
Deepshikha Singh <143700551+deepshikhaa07@users.noreply.github.com>
2024-10-18 07:50:28 +0530

    Add files via upload

---
+++ sales.sql
@@ -0,0 +1,3 @@
+SELECT distinct(transactions.currency) FROM transactions;
+select count(*) from transactions where transactions.currency="INR\r";
+SELECT distinct(transactions.currency) FROM transactions;
