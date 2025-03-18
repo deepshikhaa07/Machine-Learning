82d85a2f0cd91ac963d298f013bc81e32342ef12
Deepshikha Singh <143700551+deepshikhaa07@users.noreply.github.com>
2025-03-19 01:25:53 +0530

    Delete sales.sql

--- sales.sql
+++
@@ -1,3 +0,0 @@
-SELECT distinct(transactions.currency) FROM transactions;
-select count(*) from transactions where transactions.currency="INR\r";
-SELECT distinct(transactions.currency) FROM transactions;
